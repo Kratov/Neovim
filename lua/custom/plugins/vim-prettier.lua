c59eef34498b89582803463e12d5ae0b25773b27
kratov <mrjanzx@gmail.com>
2024-02-22 23:01:50 -0500

    Added vim-prettier

---
+++ lua/custom/plugins/vim-prettier.lua
@@ -0,0 +1,10 @@
+return {
+	"prettier/vim-prettier",                                                        -- Plugin repository
+	ft = { "javascript", "typescript", "css", "html", "json", "yaml", "markdown" }, -- File types to trigger lazy loading
+	config = function()
+		-- Plugin configuration
+		vim.g["prettier#autoformat"] = 1
+		vim.g["prettier#autoformat_require_pragma"] = 0
+	end,
+	build = "yarn install --frozen-lockfile --production", -- Build command for the plugin
+}
