d18fb3878396d5068c012669fb0e6f4f8cb730ad
Jaime Enrique Zamora Munar <jaime.zamora.-nd@disney.com>
2024-07-07 01:14:27 -0500

    added less

--- lua/custom/plugins/vim-prettier.lua
+++ lua/custom/plugins/vim-prettier.lua
@@ -1,6 +1,6 @@
 return {
 	"prettier/vim-prettier",                                                        -- Plugin repository
-	ft = { "javascript", "typescript", "css", "html", "json", "yaml", "markdown" }, -- File types to trigger lazy loading
+	ft = { "javascript", "typescript", "css", "html", "json", "yaml", "markdown", "less" }, -- File types to trigger lazy loading
 	config = function()
 		-- Plugin configuration
 		vim.g["prettier#autoformat"] = 1
