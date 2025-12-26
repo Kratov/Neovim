55d07302c8d2dbe4490b86f228e272bf3eab6f09
Jaime Zamora <github@jaimezamora.com>
2025-12-25 21:49:39 -0500

    Tag treesitter while a fix is found

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,11 +1,18 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    tag = 'v0.9.3',
     build = ':TSUpdate',
     config = function()
-      require('nvim-treesitter').setup({
-        ensure_installed = { 'lua', 'python', 'javascript', 'typescript' },
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { 'lua', 'python', 'javascript', 'typescript', 'vim', 'vimdoc', 'bash' },
+        highlight = { enable = true },
+        indent = { enable = true },
       })
     end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
   },
 }
